e9aef8f8ed15b841b22675b003eb46a6a3fa5819
rebe1637 <33025849+rebe1637@users.noreply.github.com>
2018-12-14 11:53:34 +0100

    Kan sende besked

--- tcpCLient/Program.cs
+++ tcpCLient/Program.cs
@@ -1,19 +1,43 @@
 ﻿using System;
+using System.Net.Sockets;
+using System.IO;
 
 namespace tcpCLient
 {
     class Program
     {
+        private static string message;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            TcpClient connectionSocket = new TcpClient("localhost", 6789);
+            Console.WriteLine("Client started");
+
+            Stream ns = connectionSocket.GetStream();
+            StreamWriter sw = new StreamWriter(ns);
+            StreamReader sr = new StreamReader(ns);
+            sw.AutoFlush = true;
 
 
 
+            while (true)
+            {
+                string message = Console.ReadLine();
+                sw.WriteLine(message);
+                if (message == "STOP") break;
+                string answer = sr.ReadLine();
+                Console.WriteLine("Server: " + answer);
+            }
+
+            Console.WriteLine("Client stopped, press enter");
+            string s = Console.ReadLine();
 
 
 
-
+            
         }
+
+        
     }
 }
