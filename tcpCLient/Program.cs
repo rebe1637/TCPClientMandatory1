cf5ca7fd36a5db3011c910e2bbcf2f01869c8977
rebe1637 <33025849+rebe1637@users.noreply.github.com>
2018-12-14 11:31:19 +0100

    Add project files.

---
+++ tcpCLient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tcpCLient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
